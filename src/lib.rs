9d96c8fcb25c5298aae091dff2ae8e2ab0972fc0
riojos <riojosrj@gmail.com>
2022-09-28 22:34:13 +0530

    write to json file

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 use chrono::prelude::*;
-use rocket::serde::Serialize;
+use rocket::serde::{Deserialize, Serialize};
+use std::fs::{self, OpenOptions};
+use std::io::Read;
 use std::path::PathBuf;
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
 pub struct Todo {
     #[serde(with = "my_date_format")]
@@ -46,10 +48,31 @@ pub fn utc_from(time: &str, date: PathBuf) -> DateTime<Utc> {
             .ok()
             .unwrap()
     );
-    
 
-    Utc
-        .datetime_from_str(&timestamp, "%Y-%m-%d %H:%M:%S")
+    Utc.datetime_from_str(&timestamp, "%Y-%m-%d %H:%M:%S")
         .ok()
         .unwrap()
+}
+
+/// Writes the Todo data to a json file
+pub fn add_to_json(path: &str, data: Todo) -> Vec<Todo> {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .read(true)
+        .append(true)
+        .open(path)
+        .unwrap();
+
+    // Read json file contents
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut json_contents: Vec<Todo> = serde_json::from_str(&contents).unwrap();
+
+    // Appending Todo data to the json file's contents
+    json_contents.push(data);
+
+    // Rewriting the entire data
+    fs::write(path, serde_json::to_string(&json_contents).unwrap()).unwrap();
+
+    json_contents
 }
--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,20 @@ use rocket::serde::json::Json;
 use std::path::PathBuf;
 
 #[get("/<task>/<time>/<date..>")]
-fn create(task: &str, time: &str, date: PathBuf) -> Json<Todo> {
-    Json(Todo {
+fn create(task: &str, time: &str, date: PathBuf) -> Json<Vec<Todo>> {
+    let data = Todo {
         do_by_time: utc_from(time, date),
         task: task.to_string(),
-    })
+    };
+
+    let contents = add_to_json("timings.json", data);
+    Json(contents)
 }
 
 #[get("/<id>")]
-fn read(id: i8) -> Json<Vec<Todo>> {}
+fn read(id: i8) -> Json<Vec<Todo>> {
+    todo!()
+}
 
 #[launch]
 fn launch() -> _ {

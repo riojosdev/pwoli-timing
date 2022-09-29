730168562eeb75b35539026a19020bcfb7c85eb5
riojos <riojosrj@gmail.com>
2022-09-29 13:54:53 +0530

    read route

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use pwoli_timing::*;
 use rocket::serde::json::Json;
 use std::path::PathBuf;
 
+use std::fs::OpenOptions;
+use std::io::Read;
+
 #[get("/<task>/<time>/<date..>")]
 fn create(task: &str, time: &str, date: PathBuf) -> Json<Vec<Todo>> {
     let data = Todo {
@@ -17,11 +20,24 @@ fn create(task: &str, time: &str, date: PathBuf) -> Json<Vec<Todo>> {
 }
 
 #[get("/<id>")]
-fn read(id: i8) -> Json<Vec<Todo>> {
-    todo!()
+fn read(id: usize) -> Json<Todo> {
+    let mut file = OpenOptions::new()
+        .create(true)
+        .read(true)
+        .append(true)
+        .open("timings.json")
+        .unwrap();
+
+    // Read json file contents
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut json_contents: Vec<Todo> = serde_json::from_str(&contents).unwrap();
+
+    let data = Json(json_contents.remove(id));
+    data
 }
 
 #[launch]
 fn launch() -> _ {
-    rocket::build().mount("/", routes![create])
+    rocket::build().mount("/", routes![create, read])
 }
